ed5f276e2ea04654ed771508ab121ce89de060e4
Kevin Wittek <kevin.wittek@styracosoft.de>
2017-02-02 23:38:30 +0100

    :construction: :art: Substitute Groovy docker-client with testcontainers library
    Work on #5

--- src/main/groovy/com/groovycoder/spockdockerextension/DockerClientFacade.groovy
+++ src/main/groovy/com/groovycoder/spockdockerextension/DockerClientFacade.groovy
@@ -1,56 +1,51 @@
 package com.groovycoder.spockdockerextension
 
-import de.gesellix.docker.client.DockerClient
-import de.gesellix.docker.client.DockerClientImpl
+import org.testcontainers.containers.ContainerLaunchException
+import org.testcontainers.containers.GenericContainer
+
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.Future
 
 class DockerClientFacade {
 
-    DockerClient dockerClient
+    GenericContainer dockerClient
     String image
     String name
-    Map clientSpecificContainerConfig
-    def containerHandle
 
     DockerClientFacade(Docker containerConfig) {
         image = containerConfig.image()
         name = containerConfig.name()
-        dockerClient = new DockerClientImpl()
-        clientSpecificContainerConfig = new DockerContainerConfigBuilder(containerConfig).build()
+        def latestImage = "$image:latest"
+
+        Future<String> futureImage = CompletableFuture.completedFuture(latestImage.toString())
+        dockerClient = new GenericContainer(futureImage)
+        dockerClient.setPortBindings(containerConfig.ports().toList())
     }
 
     void run() {
-        containerHandle = dockerClient.run(image, clientSpecificContainerConfig, "latest")
+        try {
+            start()
+        } catch (ContainerLaunchException e) {
+            throw new DockerRunException("Error running the container", e)
 
-        if (!status.success) {
-            throw new DockerRunException("Error running the container: $status.text")
         }
-
-        containerHandle
     }
 
     void rm() {
-        dockerClient.stop(id)
-        dockerClient.rm(id)
+        dockerClient.stop()
     }
 
     void start() {
-        dockerClient.startContainer(id)
+        dockerClient.start()
     }
 
     void stop() {
-        dockerClient.stop(id)
+        dockerClient.stop()
     }
 
     String getIp() {
-        def containerInspection = dockerClient.inspectContainer(id)
-        return containerInspection.content.NetworkSettings.Networks.bridge.IPAddress
+        //noinspection GrDeprecatedAPIUsage
+        dockerClient.containerInfo.networkSettings.ipAddress
     }
 
-    private String getId() {
-        containerHandle.container.content.Id
-    }
-
-    private getStatus() {
-        containerHandle.status.status
-    }
 }
