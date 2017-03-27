3f43abfc086dc04e61424d28a9b042fcca5fff2b
Kevin Wittek <kevin.wittek@styracosoft.de>
2017-03-27 22:42:58 +0200

    :sparkles: Change docker-compose support to testcontainers style

--- src/main/groovy/com/groovycoder/spockdockerextension/TestcontainersMethodInterceptor.groovy
+++ src/main/groovy/com/groovycoder/spockdockerextension/TestcontainersMethodInterceptor.groovy
@@ -5,6 +5,7 @@ import org.spockframework.runtime.extension.IMethodInterceptor
 import org.spockframework.runtime.extension.IMethodInvocation
 import org.spockframework.runtime.model.FieldInfo
 import org.spockframework.runtime.model.SpecInfo
+import org.testcontainers.containers.DockerComposeContainer
 import org.testcontainers.containers.GenericContainer
 import spock.lang.Shared
 
@@ -20,34 +21,55 @@ class TestcontainersMethodInterceptor extends AbstractMethodInterceptor {
     void interceptSetupSpecMethod(IMethodInvocation invocation) throws Throwable {
         def containers = findAllContainers(true)
         startContainers(containers, invocation)
+
+        def compose = findAllComposeContainers(true)
+        startComposeContainers(compose, invocation)
+
         invocation.proceed()
     }
 
-    @Override
     void interceptCleanupSpecMethod(IMethodInvocation invocation) throws Throwable {
         def containers = findAllContainers(true)
         stopContainers(containers, invocation)
+
+        def compose = findAllComposeContainers(true)
+        stopComposeContainers(compose, invocation)
+
         invocation.proceed()
     }
-
 
     @Override
     void interceptSetupMethod(IMethodInvocation invocation) throws Throwable {
         def containers = findAllContainers(false)
         startContainers(containers, invocation)
+
+        def compose = findAllComposeContainers(false)
+        startComposeContainers(compose, invocation)
+
         invocation.proceed()
     }
+
 
     @Override
     void interceptCleanupMethod(IMethodInvocation invocation) throws Throwable {
         def containers = findAllContainers(false)
         stopContainers(containers, invocation)
+
+        def compose = findAllComposeContainers(false)
+        stopComposeContainers(compose, invocation)
+
         invocation.proceed()
     }
 
     private List<FieldInfo> findAllContainers(boolean shared) {
         spec.allFields.findAll { FieldInfo f ->
-            GenericContainer.isAssignableFrom(f.type) &&  f.isAnnotationPresent(Shared) == shared
+            GenericContainer.isAssignableFrom(f.type) && f.shared == shared
+        }
+    }
+
+    private List<FieldInfo> findAllComposeContainers(boolean shared) {
+        spec.allFields.findAll { FieldInfo f ->
+            DockerComposeContainer.isAssignableFrom(f.type) && f.shared == shared
         }
     }
 
@@ -66,6 +88,21 @@ class TestcontainersMethodInterceptor extends AbstractMethodInterceptor {
             container.start()
         }
     }
+
+    private static void startComposeContainers(List<FieldInfo> compose, IMethodInvocation invocation) {
+        compose.each { FieldInfo f ->
+            DockerComposeContainer c = f.readValue(invocation.instance) as DockerComposeContainer
+            c.starting(null)
+        }
+    }
+
+    private static void stopComposeContainers(List<FieldInfo> compose, IMethodInvocation invocation) {
+        compose.each { FieldInfo f ->
+            DockerComposeContainer c = f.readValue(invocation.instance) as DockerComposeContainer
+            c.finished(null)
+        }
+    }
+
 
     private static GenericContainer readContainerFromField(FieldInfo f, IMethodInvocation invocation) {
         f.readValue(invocation.instance) as GenericContainer
